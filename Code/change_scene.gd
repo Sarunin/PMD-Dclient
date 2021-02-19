8ef1d3b06a9c71a38305365de0dfc0f80b10dd02
Sarunin <76625198+Sarunin@users.noreply.github.com>
2021-02-19 23:22:53 +0100

    corrections

--- Code/change_scene.gd
+++ Code/change_scene.gd
@@ -3,28 +3,22 @@ extends Button
 export(String) var tag = ""
 
 onready var thisButton = get_node(".")
-var scene_backup = PackedScene.new()
-var scene_loadup = load("res://Scenes/GameScene.tscn")
-var pause = false
+onready var pauseMenu_pause = get_node("../../Pause")
+onready var pauseMenu_run = get_node("../Pause")
 
 func _ready():
 	thisButton.connect("pressed", self, "_button_pressed")
-	scene_backup.pack(get_tree().get_scene("res://Scenes/GameScene.tscn"))
 
 func _button_pressed():
 	match tag:
 		"GameMenu": 
-			
-			if pause == false:
-				if get_tree().change_scene("res://Scenes/GameScene.tscn") != OK:
-					print("Loading Failure")
-			else:
-				if get_tree().change_scene("res://Scenes/backup.tscn") != OK:
-					print("Loading Failure")
-		"MainMenu": 
-			pause = true
-			scene_backup.pack(get_tree().get_current_scene())
-			ResourceSaver.save("res://Scenes/backup.tscn", scene_backup)
+			if get_tree().change_scene("res://Scenes/GameScene.tscn") != OK:
+				print("Loading Failure")
+		"MainMenu":
 			if get_tree().change_scene("res://Scenes/MainMenu.tscn") != OK:
 				print("Loading Failure")
+		"PauseButton":
+			pauseMenu_run.visible = true
+		"ResumeButton":
+			pauseMenu_pause.visible = false
 		"ExitButton": get_tree().quit()
