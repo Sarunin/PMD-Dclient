8a6e8acf102251650fc59d069260c04c2ff76227
Aurora <programming.aurora@gmail.com>
2021-03-13 18:56:45 +0000

    Added propper .json file extension to pokedex database.

--- code/extra/database_manager.gd
+++ code/extra/database_manager.gd
@@ -4,7 +4,7 @@ var pokemon
 
 func _ready():
 	var f = File.new()
-	f.open("res://resources/data/pokedex.res", 1) # 1 to read
+	f.open("res://resources/data/pokedex.json", 1) # 1 to read
 	pokemon = parse_json(f.get_as_text())
 	f.close()
 
