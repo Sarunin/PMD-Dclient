4d22ab508bdc4edec506cbb62614cffc11569cb2
Aurora <programming.aurora@gmail.com>
2021-03-30 02:12:23 +0100

    Added base move class

---
+++ code/game/moves/move.gd
@@ -0,0 +1,24 @@
+class_name Move
+
+var name
+var name_id
+var description
+var power
+var accuracy
+var sp_cost
+var effect
+var type
+var category
+var move_type
+var move_range
+var priority
+var contact
+var snatchable
+var defrosts
+var reflected
+var blocked
+var mirrored
+var king_rock
+
+func execute():
+	pass
